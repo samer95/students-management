ad4f81e3c4752885c466977167f191180a2817d0
samerlahlouh <samer.lahlouh.95@hotmail.com>
2020-06-20 19:55:20 +0300

    prepare tabs

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { Component } from 'react';
+import { Tab, Tabs } from "react-bootstrap";
 import './App.css';
 
-function Index() {
-  return (
-    <div>
+const TABS = [
+  { key: 'text', title: 'Metin Dosyası Bazlı', component: null },
+  { key: 'excel', title: 'Excel Dosyası Bazlı', component: null },
+  { key: 'database', title: 'Veritabanı Dosyası Bazlı', component: null },
+]
+
+class Index extends Component {
+  render = () => {
+    return <div className="container mt-3">
+      <Tabs id="main-tabs" defaultActiveKey={TABS[0].key}>
+        {TABS.map(tab =>
+            <Tab eventKey={tab.key} title={tab.title} key={tab.key}>
+              {tab.component}
+            </Tab>
+        )}
+      </Tabs>
     </div>
-  );
+  }
 }
 
 export default Index;
