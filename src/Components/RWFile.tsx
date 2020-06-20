22e98a691a7f8b6a03b2e703a0d363b85dbed865
samerlahlouh <samer.lahlouh.95@hotmail.com>
2020-06-20 23:00:52 +0300

    extract RWFile component

---
+++ src/Components/RWFile.tsx
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import { Button, Col, Form, Row, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import Student from "../Instances/Student";
+
+const COLS: { key: string, value: string }[] = [
+  { key: 'id', value: 'Öğrenci Numarası' },
+  { key: 'fName', value: 'Adı' },
+  { key: 'lName', value: 'Soyadı' },
+  { key: 'visa', value: 'Vize Notu' },
+  { key: 'final', value: 'final' },
+  { key: 'succGrade', value: 'Başarı Notu' },
+  { key: 'succChar', value: 'Harf Notu' },
+]
+
+interface IState {
+  data: any,
+  calculatedData: any,
+}
+
+interface IProps {
+  uploadData: any,
+  downloadData: any,
+}
+
+const INIT_STATE = {
+  data: null,
+  calculatedData: null,
+}
+
+class RWFile extends Component<IProps, IState> {
+  constructor({ props }: { props: any }) {
+    super(props)
+    this.state = INIT_STATE
+    this.calcGrades = this.calcGrades.bind(this)
+  }
+
+  uploadFile = (e: any) => {
+    e.preventDefault()
+    this.props.uploadData(
+        e.target.files[0],
+        (data: []) => this.setState({ data })
+    )
+  }
+
+  downloadCalculatedDataFile = (e: any) => {
+    e.preventDefault()
+    this.props.downloadData(this.state.calculatedData)
+  }
+
+  calcGrades() {
+    let { data } = this.state
+    const calculatedData = data.map((row: Student) => {
+      let succGrade = 0.4 * row.visa + 0.6 * row.final
+      succGrade = parseFloat(succGrade.toFixed(2))
+      let succChar
+      if (succGrade >= 0 && succGrade <= 24) {
+        succChar = 'D'
+      } else if (succGrade >= 25 && succGrade <= 49) {
+        succChar = 'C'
+      } else if (succGrade >= 50 && succGrade <= 74) {
+        succChar = 'B'
+      } else if (succGrade >= 75 && succGrade <= 100) {
+        succChar = 'A'
+      }
+      return { ...row, succGrade, succChar }
+    })
+    this.setState({ calculatedData })
+  }
+
+  render() {
+    const { data, calculatedData } = this.state
+    return <div className="mt-2">
+      {!data && (
+          <Form.File
+              onChange={this.uploadFile}
+              className="mb-2"
+              label="Kayıtları Yükle"
+          />
+      )}
+
+      {data && (<>
+        <Row className="mb-3">
+          <Col>
+            {!calculatedData && (
+                <Button variant="primary" onClick={this.calcGrades}>
+                  Başarı Notlarını Hesapla
+                </Button>
+            )}
+            {calculatedData && (
+                <div className="d-flex flex-row">
+                  <Button
+                      variant="secondary"
+                      onClick={this.downloadCalculatedDataFile}
+                  >
+                    Dışarı Aktar
+                  </Button>
+                  <Button
+                      className="ml-2"
+                      variant="outline-danger"
+                      onClick={() => this.setState({ calculatedData: null })}
+                  >
+                    Notları Sıfırla
+                  </Button>
+                </div>
+            )}
+          </Col>
+          <Col>
+            <Button
+                className="float-right"
+                variant="danger"
+                onClick={() => this.setState(INIT_STATE)}
+            >
+              Kayıtları Sıfırla
+              <FontAwesomeIcon icon={faTimes} className="ml-2"/>
+            </Button>
+          </Col>
+        </Row>
+        <Table striped bordered hover>
+          <thead>
+          <tr>
+            {COLS.map(col => <th key={col.key}>{col.value}</th>)}
+          </tr>
+          </thead>
+          <tbody>
+          {(calculatedData || data).map((row: any, rowIndex: number) => (
+              <tr key={rowIndex}>
+                {COLS.map(col => <td key={col.key}>{row[col.key] || ''}</td>)}
+              </tr>
+          ))}
+          </tbody>
+        </Table>
+      </>)}
+    </div>
+  }
+}
+
+export default RWFile
--- src/Components/RWTextFile.tsx
+++ src/Components/RWTextFile.tsx
@@ -1,38 +1,9 @@
-import React, { Component } from 'react';
-import { Button, Col, Form, Row, Table } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import React, { Component, Fragment } from 'react';
 import Student from "../Instances/Student";
+import RWFile from "./RWFile";
 
-const COLS: { key: string, value: string }[] = [
-  { key: 'id', value: 'Öğrenci Numarası' },
-  { key: 'fName', value: 'Adı' },
-  { key: 'lName', value: 'Soyadı' },
-  { key: 'visa', value: 'Vize Notu' },
-  { key: 'final', value: 'final' },
-  { key: 'succGrade', value: 'Başarı Notu' },
-  { key: 'succChar', value: 'Harf Notu' },
-]
-
-interface IState {
-  data: any,
-  calculatedData: any,
-}
-
-const INIT_STATE = {
-  data: null,
-  calculatedData: null,
-}
-
-class RWTextFile extends Component<{}, IState> {
-  constructor({ props }: { props: any }) {
-    super(props)
-    this.state = INIT_STATE
-    this.calcGrades = this.calcGrades.bind(this)
-  }
-
-  uploadFile = (e: any) => {
-    e.preventDefault()
+class RWTextFile extends Component {
+  uploadData = (file: any, callback: any) => {
     const reader = new FileReader()
     reader.onload = e => {
       if (e.target?.result) {
@@ -49,17 +20,15 @@ class RWTextFile extends Component<{}, IState> {
           }
           return student
         })
-        this.setState({ data })
+        callback(data)
       }
     };
-    reader.readAsText(e.target.files[0])
+    reader.readAsText(file)
   }
 
-  downloadCalculatedDataFile = (e: any) => {
-    e.preventDefault()
+  downloadData = (data: []) => {
     const dataToPrint =
-        this.state.calculatedData
-        .reduce((rowsText: string, row: Student) => (
+        data.reduce((rowsText: string, row: Student) => (
             `${rowsText}\n${Object.values(row).join(" ")}`
         ), '')
     const element = document.createElement("a")
@@ -70,87 +39,15 @@ class RWTextFile extends Component<{}, IState> {
     element.click()
   }
 
-  calcGrades() {
-    let { data } = this.state
-    const calculatedData = data.map((row: Student) => {
-      let succGrade = 0.4 * row.visa + 0.6 * row.final
-      succGrade = parseFloat(succGrade.toFixed(2))
-      let succChar
-      if (succGrade >= 0 && succGrade <= 24) {
-        succChar = 'D'
-      } else if (succGrade >= 25 && succGrade <= 49) {
-        succChar = 'C'
-      } else if (succGrade >= 50 && succGrade <= 74) {
-        succChar = 'B'
-      } else if (succGrade >= 75 && succGrade <= 100) {
-        succChar = 'A'
-      }
-      return { ...row, succGrade, succChar }
-    })
-    this.setState({ calculatedData })
-  }
-
   render() {
-    const { data, calculatedData } = this.state
-    return <div className="mt-2">
-      {!data && (
-          <Form.File
-              onChange={this.uploadFile}
-              className="mb-2"
-              label="Kayıtları Yükle"
+    return (
+        <Fragment>
+          <RWFile
+              uploadData={this.uploadData}
+              downloadData={this.downloadData}
           />
-      )}
-
-      {data && (<>
-        <Row className="mb-3">
-          <Col>
-            {!calculatedData && (
-                <Button variant="primary" onClick={this.calcGrades}>
-                  Başarı Notlarını Hesapla
-                </Button>
-            )}
-            {calculatedData && (
-                <div className="d-flex flex-row">
-                  <Button variant="secondary" onClick={this.downloadCalculatedDataFile}>
-                    Dışarı Aktar
-                  </Button>
-                  <Button
-                      className="ml-2"
-                      variant="outline-danger"
-                      onClick={() => this.setState({ calculatedData: null })}
-                  >
-                    Notları Sıfırla
-                  </Button>
-                </div>
-            )}
-          </Col>
-          <Col>
-            <Button
-                className="float-right"
-                variant="danger"
-                onClick={() => this.setState(INIT_STATE)}
-            >
-              Kayıtları Sıfırla
-              <FontAwesomeIcon icon={faTimes} className="ml-2"/>
-            </Button>
-          </Col>
-        </Row>
-        <Table striped bordered hover>
-          <thead>
-          <tr>
-            {COLS.map(col => <th key={col.key}>{col.value}</th>)}
-          </tr>
-          </thead>
-          <tbody>
-          {(calculatedData || data).map((row: any, rowIndex: number) => (
-              <tr key={rowIndex}>
-                {COLS.map(col => <td key={col.key}>{row[col.key] || ''}</td>)}
-              </tr>
-          ))}
-          </tbody>
-        </Table>
-      </>)}
-    </div>
+        </Fragment>
+    )
   }
 }
 
