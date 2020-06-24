90235cfe8f263af0eaa49fb66819554cae2c4b77
samerlahlouh <samer.lahlouh.95@hotmail.com>
2020-06-24 17:32:59 +0300

    server: change port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
-const PORT = 3000;
+const PORT = 3001;
 
 // database configurations
 const connection = mysql.createConnection({
