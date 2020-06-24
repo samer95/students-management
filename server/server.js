e3aee7e9931c914cc6c5566350822485109a1e4c
samerlahlouh <samer.lahlouh.95@hotmail.com>
2020-06-24 17:18:09 +0300

    config server & connection with mySQL

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
+const PORT = 3000;
+
+// database configurations
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'sam'
+});
+connection.connect();
+
+// app configurations
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.listen(PORT);
+console.log('API server started on: ' + PORT);
+
+// routes configurations
+const routes = function(app) {
+  app.route('/students').get(function(req, res) {
+    connection.query("SELECT * FROM students", function (err, students) {
+      res.send(students);
+    });
+  })
+};
+routes(app); //register the routes
